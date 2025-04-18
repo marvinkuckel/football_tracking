4c85d10ce4aea770a4346c1098db6930f0c51076
Marc-Maxelon <marc.maxelon@study.hs-duesseldorf.de>
2025-04-18 21:56:01 +0200

    Implement Shi-Tomasi

--- pipeline/opticalflow.py
+++ pipeline/opticalflow.py
@@ -5,20 +5,38 @@ class OpticalFlow:
     def __init__(self):
         self.name = "Optical Flow" # Do not change the name of the module as otherwise recording replay would break!
         self.pre_image = None
+        self.pre_points = None
 
     def start(self, data):
         self.pre_image = None
+        self.pre_points = None
 
     def stop(self, data):
         self.pre_image = None
+        self.pre_points = None
 
     def step(self, data):
         # TODO: Implement processing of a single frame
         frame = data["image"]
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+        if self.pre_image is None:
+            self.pre_image = gray
+            return {
+                "opticalFlow": np.array([0.0, 0.0], dtype = np.float32)
+            }
+
+        if self.pre_points is None:
+            self.pre_points = cv2.goodFeaturesToTrack(
+                self.pre_image,  # Shi-Tomasi
+                maxCorners = 100,
+                qualityLevel = 0.3,
+                minDistance = 7,
+                blockSize = 7
+            )
+
         self.pre_image = gray if self.pre_image is None else self.pre_image
-        
+
         # The task of the optical flow module is to determine the overall avergae pixel shift between this and the previous image. 
         # You 
 
